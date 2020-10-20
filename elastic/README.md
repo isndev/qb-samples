81e83a4ef76d7f3d4f5b289f37a9e4e47a476a97
Mohamed, Belhadi <mbelhadi@isndev.com>
2020-10-20 04:02:28 +0200

    [dev] use config json config file for server, added sample files, updated README

--- elastic/README.md
+++ elastic/README.md
@@ -1,12 +1,10 @@
-# qb-samples
-## Elastic
-Sample demonstrating how to use qb to create a (simple) centralized log database with it's dedicated log pusher aka [ELK](https://www.elastic.co/what-is/elk-stack)
-* ### client-log-watcher
-
+# qb-samples/elastic
+Sample demonstrating how to use qb to create a (simple) centralized log database with it's dedicated log pusher ala [ELK](https://www.elastic.co/what-is/elk-stack)
+## Client-log-watcher
 Client will
-* watch a log file
-* parse and convert a log line from user format in json
-* send json object(s) to the server
+- watch a log file
+- parse and convert a log line from user format in json
+- send json object(s) to the server
 
 Usage
 ```sh
@@ -17,7 +15,7 @@ Usage
 * **Host** remote host (default:localhost)
 * **Port** remote port (default:60123)
 
-* ### server
+## Server
 
 Server will
 * receive logs from watchers
@@ -26,7 +24,39 @@ Server will
 
 Usage
 ```sh
-# ./qb-sample-elastic-server
+# ./qb-sample-elastic-server [CONFIG_FILE]
 ```
 
-###todo
+* **Config File** <br>
+  - **N** JSONAcceptor - listen [interface:port], accept log watchers and forward sessions using a naive round robin to JSONSessionHandler(s)
+  - **N** JSONSessionHandler - receive logs asynchronously and forward them to Database
+  - **N** HTTPAcceptor - listen [interface:port], accept and forward sessions using a naive round robin to HTTPSessionHandler(s)
+  - **N** HTTPSessionHandler - receive http requests, ask to database and send back requested logs
+  - Database - store logs in memory
+ex: 2 cores configuration
+```json
+{
+  "JSONAcceptor": [
+    {
+      "core": 0,
+      "listen": "0.0.0.0",
+      "port": 60123
+    }
+  ],
+  "HTTPAcceptor": [
+    {
+      "core": 0,
+      "listen": "0.0.0.0",
+      "port": 8080
+    }
+  ],
+  "JSONSessionHandler": [{"core": 0}],
+  "HTTPSessionHandler": [{"core": 0}],
+  "Database": [{"core": 1}]
+}
+```
+## Ask for Data
+ex: curl asking for full database
+```sh
+# curl http://localhost:8080/get
+```
--- elastic/server/main.cpp
+++ elastic/server/main.cpp
@@ -15,6 +15,7 @@
  *         limitations under the License.
  */
 
+#include <fstream>
 #include <qb/main.h>
 #include "AcceptActor.h"
 #include "SessionJSONActor.h"
@@ -22,39 +23,57 @@
 #include "DatabaseActor.h"
 
 int
-main(int, char *argv[]) {
+main(int argc, char *argv[]) {
     // (optional) initialize the logger
     qb::io::log::init(argv[0]); // filepath
     qb::io::log::setLevel(
-        qb::io::log::Level::WARN); // log only warning, error an critical
+        qb::io::log::Level::INFO); // log only info, warning, error an critical
 
     // configure the Core
     qb::Main main;
+    qb::ActorIdList db_ids;
+    qb::ActorIdList json_ids;
+    qb::ActorIdList http_ids;
+    // parse config
+    try {
+        std::ifstream config_file(argc > 1 ? argv[1] : "config_multi.json");
+        qb::json::object config;
 
-    // core db
-    auto db_ids = main.core(1).setLatency(100)
-                      .builder()
-        .addActor<DatabaseActor>()
-        .idList();
+        config_file >> config;
+        // databases json
+        for (auto it : config["Database"])
+            db_ids.push_back(main.addActor<DatabaseActor>(it["core"].get<qb::CoreId>()));
+        // session handler json
+        for (auto it : config["JSONSessionHandler"])
+            json_ids.push_back(main.addActor<SessionJSONActor>(it["core"].get<qb::CoreId>(), db_ids));
+        // session handler http
+        for (auto it : config["HTTPSessionHandler"])
+            http_ids.push_back(main.addActor<SessionHTTPActor>(it["core"].get<qb::CoreId>(), db_ids));
+        // acceptor json
+        for (auto it : config["JSONAcceptor"]) {
+            main.core(it["core"].get<qb::CoreId>())
+                .addActor<AcceptActor>(it["listen"].get<std::string>(),
+                                            it["port"].get<uint16_t>(),
+                                            json_ids);
+        }
+        // acceptor http
+        for (auto it : config["HTTPAcceptor"]) {
+            main.core(it["core"].get<qb::CoreId>())
+                .addActor<AcceptActor>(it["listen"].get<std::string>(),
+                                            it["port"].get<uint16_t>(),
+                                            http_ids);
+        }
+    } catch (...) {
+        std::cerr << "failed to parse config file" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    // core io
-    main.core(0).setLatency(100);
-    // json feed part
-    auto json_ids = main.core(0).builder()
-        .addActor<SessionJSONActor>(db_ids)
-        .idList();
-    // http api part
-    auto http_ids = main.core(0).builder()
-        .addActor<SessionHTTPActor>(db_ids)
-        .idList();
-
-    // acceptors
-    main.core(0).builder()
-        .addActor<AcceptActor>(std::string("127.0.0.1"), 60123, json_ids)
-        .addActor<AcceptActor>(std::string("127.0.0.1"), 8080, http_ids);
+    for (auto core_id : main.usedCoreSet()) {
+        main.core(core_id).setLatency(100);
+    }
 
     main.start(); // start the engine asynchronously
     main.join();  // Wait for the running engine
     // if all my actors had been destroyed then it will release the wait !
-    return 0;
+    return main.hasError() ? EXIT_FAILURE : EXIT_SUCCESS;
 }
